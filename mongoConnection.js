cba5925efc417e76867899c8aa7944b8133879fb
mathiasms <mathiasmedinasanchez11@gmail.com>
2017-06-15 17:40:40 -0300

    Mongo connection

--- mongoConnection.js
+++ mongoConnection.js
@@ -1,11 +1,51 @@
-/*var mongo = require('mongodb');
+
+var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
-var url = 'mongodb://localhost:27017/alvadb';
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
+insertData = function(db, data){
+db.collection('measurements').insert({
+"currentValue" : data
+}, function(err, result){
+assert.equal(err, null);
+console.log("Inserted the measurement: "+ data +" into the measurements collections");
 });
+}
 
-module.exports = MongoClient;*/
+insertCurrentData = function(db, currentValue, currentType, measurementDate){
+db.collection('measurements').insert({
+"currentValue" : currentValue,
+"currentType" : currentType,
+"measurementDate" : measurementDate
+}, function(err, result){
+assert.equal(err, null);
+console.log("Inserted the measurement into the measurements collections");
+});
+}
+
+findAllData = function(db){
+var measurements = db.collection('measurements').find();
+return measurements;
+}
+
+exports.insert = function(data) {
+MongoClient.connect(url, function(err, db) {
+ assert.equal(err, null);
+ insertData(db, data);
+});
+};
+
+exports.insertCurrentData = function(data) {
+MongoClient.connect(url, function(err, db) {
+ assert.equal(err, null);
+ insertCurrentData(db, data.currentValue, data.currentType, data.measurementDate);
+});
+};
+
+exports.findAll = function(callback) {
+MongoClient.connect(url, function(err, db) {
+ assert.equal(err, null);
+ callback(findAllData(db));
+});
+};
